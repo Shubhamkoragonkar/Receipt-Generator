eccbac7de89602f19d0bc9efeb55b7b93a11eea0
Shubham Korgaonkar <153352443+Shubhamkoragonkar@users.noreply.github.com>
2023-12-20 00:56:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let receiptItems = [];
-const shopName = "General Store"; // Replace 'Your Shop Name' with your shop's name
+const shopName = "Laxmi Decorators"; // Replace 'Your Shop Name' with your shop's name
 
 function addItem() {
   const itemName = document.getElementById('itemName').value;
@@ -32,34 +32,51 @@ function displayReceipt() {
 }
 
 function generatePDF() {
-    const filename = 'receipt.pdf';
-    let itemsTable = '<table style="width:100%; border-collapse: collapse; margin-bottom: 20px; border: 1px solid #000;"><tr><th style="text-align:left; padding: 8px; border-bottom: 1px solid #000;">Sr. No.</th><th style="text-align:left; padding: 8px; border-bottom: 1px solid #000; width: 40%;">Item</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Quantity</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Price (₹)</th></tr>';
+  const filename = 'receipt.pdf';
+  let itemsTable = '<table style="width:100%; border-collapse: collapse; margin-bottom: 20px; border: 1px solid #000;">';
+
+  // Rest of your table creation code
+  itemsTable += '<tr><th style="text-align:left; padding: 8px; border-bottom: 1px solid #000;">Sr. No.</th><th style="text-align:left; padding: 8px; border-bottom: 1px solid #000; width: 40%;">Item</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Quantity</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Price Per Piece(₹)</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Amount(₹)</th></tr>';
+
+  let totalAmount = 0;
+  receiptItems.forEach((item, index) => {
+    const serialNumber = index + 1;
+    totalAmount += item.price * item.quantity;
+    itemsTable += `<tr><td style="padding: 8px; border-bottom: 1px solid #000;">${serialNumber}</td><td style="padding: 8px; border-bottom: 1px solid #000;">${item.name}</td><td style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">${item.quantity}</td><td style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">₹${item.price.toFixed(2)}</td><td style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">₹${(item.price * item.quantity).toFixed(2)}</td></tr>`;
+  });
   
-    let totalAmount = 0;
-    receiptItems.forEach((item, index) => {
-      const serialNumber = index + 1;
-      totalAmount += item.price * item.quantity;
-      itemsTable += `<tr><td style="padding: 8px; border-bottom: 1px solid #000;">${serialNumber}</td><td style="padding: 8px; border-bottom: 1px solid #000;">${item.name}</td><td style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">${item.quantity}</td><td style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">₹${item.price.toFixed(2)}</td></tr>`;
-    });
+  itemsTable += `<tr style="background-color: rgb(223, 223, 223);"><td colspan="3" style="text-align:right; padding: 8px;"><strong>Total:</strong></td><td colspan = "2" style="text-align:center; padding: 8px; border-top: 1px solid #000;"><strong>₹${totalAmount.toFixed(2)}</strong></td></tr>`;
+  itemsTable += '</table>';
   
-    itemsTable += `<tr><td colspan="3" style="text-align:right; padding: 8px;"><strong>Total:</strong></td><td style="text-align:right; padding: 8px; border-top: 1px solid #000;"><strong>₹${totalAmount.toFixed(2)}</strong></td></tr>`;
-    itemsTable += '</table>';
-  
-    const shopHeader = `<h1 style="text-align: center; font-size: 36px; margin-bottom: 10px; color: black; text-shadow: 2px 2px 4px #000;">${shopName}</h1>`;
-    const shopDetails = `<p><strong>Address:</strong> Your Shop Address<br><strong>Owner:</strong> XXXXX<br><strong>Phone Number:</strong>XXXXXXXXXX</p>`;
-  
-    const content = `<html><head><title>${shopName}</title><style>
-      body { font-family: Arial, sans-serif; margin: 20px; }
-      table { width: 100%; border-collapse: collapse; }
-      th, td { padding: 10px; border: 1px solid #000; }
-      th { background-color: #f2f2f2; }
-    </style></head><body>${shopHeader}${shopDetails}${itemsTable}</body></html>`;
-  
-    const printWindow = window.open('', '_blank');
-    printWindow.document.open();
-    printWindow.document.write(content);
-    printWindow.document.close();
-  
-    printWindow.print();
-  }
-  
+  let name = prompt("What is customers name")
+  let phno = prompt("What is customers Phone Number")
+  let add = prompt("What is customers address")
+  const image = `<img src="android-chrome-512x512.png" alt="logo"/>`;
+  const shopHeader = `<h1 style="text-align: center; font-size: 36px; margin-bottom: 10px; color: black; text-shadow: 2px 2px 4px #000;">${shopName}</h1>`;
+  const shopDetails = `<p><strong>Address:</strong> B/3 Mahesh Nagar Near Hanuman Mandir Nalasopara East -401303<br><strong>Owner:</strong> SURESH NARAYAN KORGAONKAR<br><strong>Phone Number:</strong> 9892023888</p>`;
+  const currentDate = new Date().toLocaleDateString();
+  const customerDetails = `
+    <div style="display:flex;justify-content:left; margin:10px 10px;flex-wrap:wrap">
+      <p><strong>Bill to:</strong><br> ${name}<br>
+      ${phno}<br>
+      ${add}</p>
+    </div>
+  `;
+  const dateDiv = `<div style="display:flex;justify-content:right; margin:10px 10px;">Date: ${currentDate}</div>`;
+
+  const content = `<html><head><title>${shopName}</title><style>
+    body { font-family: Arial, sans-serif; margin: 20px; }
+    div { display: flex; gap: 5px; justify-content: center; }
+    div img { display: flex; width: 50px; height: 50px; margin: 13px 5px; padding: 5px 0px; }
+    p { line-height: 30px; }
+    table { width: 100%; border-collapse: collapse; }
+    th, td { padding: 10px; border: 1px solid #000; }
+    th { background-color: #f2f2f2; }
+  </style></head><body><div>${image} ${shopHeader}</div>${shopDetails} ${dateDiv} ${itemsTable} ${customerDetails}</body></html>`;
+   
+  const printWindow = window.open('', '_blank');
+  printWindow.document.open();
+  printWindow.document.write(content);
+  printWindow.document.close();
+  printWindow.print();
+}
