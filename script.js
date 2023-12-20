60aed78e15831c98109725b8fc3c1fdd99b720ab
Shubham Korgaonkar <153352443+Shubhamkoragonkar@users.noreply.github.com>
2023-12-20 14:04:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+
+function showItemForm() {
+  // Hide the customer details section
+  const customerDetails = document.getElementById('customerDetails');
+  customerDetails.style.display = 'none';
+  
+  // Show the item details section
+  const itemDetails = document.getElementById('itemDetails');
+  itemDetails.style.display = 'block';
+}
+
 let receiptItems = [];
-const shopName = "Laxmi Decorators"; // Replace 'Your Shop Name' with your shop's name
+const shopName = "Laxmi Decoraters"; // Replace 'Your Shop Name' with your shop's name
 
 function addItem() {
   const itemName = document.getElementById('itemName').value;
@@ -34,7 +45,7 @@ function displayReceipt() {
 function generatePDF() {
   const filename = 'receipt.pdf';
   let itemsTable = '<table style="width:100%; border-collapse: collapse; margin-bottom: 20px; border: 1px solid #000;">';
-
+  
   // Rest of your table creation code
   itemsTable += '<tr><th style="text-align:left; padding: 8px; border-bottom: 1px solid #000;">Sr. No.</th><th style="text-align:left; padding: 8px; border-bottom: 1px solid #000; width: 40%;">Item</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Quantity</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Price Per Piece(₹)</th><th style="text-align:right; padding: 8px; border-bottom: 1px solid #000;">Amount(₹)</th></tr>';
 
@@ -48,22 +59,21 @@ function generatePDF() {
   itemsTable += `<tr style="background-color: rgb(223, 223, 223);"><td colspan="3" style="text-align:right; padding: 8px;"><strong>Total:</strong></td><td colspan = "2" style="text-align:center; padding: 8px; border-top: 1px solid #000;"><strong>₹${totalAmount.toFixed(2)}</strong></td></tr>`;
   itemsTable += '</table>';
   
-  // let name = prompt("What is customers name")
-  // let phno = prompt("What is customers Phone Number")
-  // let add = prompt("What is customers address")
+  var cname = document.getElementById("customerName").value
+  var cph = document.getElementById("customerPhone").value
+  var cadd = document.getElementById("customerAddress").value
+
   const image = `<img src="android-chrome-512x512.png" alt="logo"/>`;
   const shopHeader = `<h1 style="text-align: center; font-size: 36px; margin-bottom: 10px; color: black; text-shadow: 2px 2px 4px #000;">${shopName}</h1>`;
   const shopDetails = `<p><strong>Address:</strong> B/3 Mahesh Nagar Near Hanuman Mandir Nalasopara East -401303<br><strong>Owner:</strong> SURESH NARAYAN KORGAONKAR<br><strong>Phone Number:</strong> 9892023888</p>`;
   const currentDate = new Date().toLocaleDateString();
   const customerDetails = `
-    <div style="display:flex;justify-content:left; margin:10px 10px;flex-wrap:wrap">
-      <p><strong>Bill to:</strong><br> name<br>
-      phno<br>
-      add</p>
-    </div>
+  <div style="display:flex;justify-content:left; margin:5px 10px;flex-wrap:wrap;gap:30px">
+  <p><strong>Bill to:</strong><p>${cname}</p><p>${cph}</p><p>${cadd}</p></p>
+  </div>
   `;
-  const dateDiv = `<div style="display:flex;justify-content:right; margin:10px 10px;">Date: ${currentDate}</div>`;
-
+  const dateDiv = `<div style="display:flex;justify-content:right; margin:px 5px 10px;">Date: ${currentDate}</div>`;
+  
   const content = `<html><head><title>${shopName}</title><style>
     body { font-family: Arial, sans-serif; margin: 20px; }
     div { display: flex; gap: 5px; justify-content: center; }
@@ -72,7 +82,7 @@ function generatePDF() {
     table { width: 100%; border-collapse: collapse; }
     th, td { padding: 10px; border: 1px solid #000; }
     th { background-color: #f2f2f2; }
-  </style></head><body><div>${image} ${shopHeader}</div>${shopDetails} ${dateDiv} ${itemsTable} ${customerDetails}</body></html>`;
+  </style></head><body><div>${image} ${shopHeader}</div>${shopDetails} ${dateDiv} ${customerDetails}${itemsTable}</body></html>`;
    
   const printWindow = window.open('', '_blank');
   printWindow.document.open();
